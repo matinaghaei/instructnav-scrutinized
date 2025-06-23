ec30f9b9b6cd72546d087ac8984095288273decf
Matin Aghaei <m.jaffaraghaei@gmail.com>
2025-06-23 12:35:32 -0400

    Modify dataset.episodes directly

--- per_scene_dataset.py
+++ per_scene_dataset.py
@@ -1,41 +1,23 @@
-from habitat.core.dataset import Dataset
+import habitat
 from collections import defaultdict
-from habitat.core.dataset import EpisodeIterator, Episode
+from omegaconf import DictConfig
+from habitat.core.registry import registry
+from config_utils import HM3D_CONFIG_PATH
+
+config = habitat.get_config(HM3D_CONFIG_PATH)
+_dataset = registry.get_dataset(
+    name=config.habitat.dataset.type
+)
 
 
-class PerSceneDataset(Dataset):
-    def __init__(self, config, *args, **kwargs):
-        super().__init__(config, *args, **kwargs)
-        self.episodes_per_scene = config.episodes_per_scene
-    def get_episode_iterator(self, *args, **kwargs):
-        return LimitingEpisodeIterator(
-            episodes=self.episodes,
-            episodes_per_scene=self.episodes_per_scene,
-            *args,
-            **kwargs,
-        )
+class PerSceneDataset(_dataset):
 
-
-class LimitingEpisodeIterator(EpisodeIterator):
-    """EpisodeIterator that caps the number of episodes per scene."""
-    def __init__(
-        self,
-        episodes: list[Episode],
-        episodes_per_scene: int,
-        *args,
-        **kwargs
-    ):
-        # 1. Group episodes by scene_id
+    def __init__(self, config: DictConfig):
+        super().__init__(config)
         scenes = defaultdict(list)
-        for ep in episodes:
+        for ep in self.episodes:
             scenes[ep.scene_id].append(ep)
-        # 2. Sample up to episodes_per_scene per scene
-        filtered_eps = []
+        filtered = []
         for eps in scenes.values():
-            filtered_eps.extend(eps[:episodes_per_scene])  # or use random.sample(eps, episodes_per_scene)
-        # 3. Initialize the base iterator with filtered episodes
-        super().__init__(
-            filtered_eps,
-            *args,
-            **kwargs
-        )
+            filtered.extend(eps[:config.episodes_per_scene])
+        self.episodes = filtered
