1e1484584dbb3a5239a54fa99da257bb5e4e3748
Matin <m.jaffaraghaei@gmail.com>
2025-09-05 18:01:03 -0400

    Remove frontier points within 1.0 meter distance of the current position

--- mapper.py
+++ mapper.py
@@ -288,7 +288,7 @@ class Instruct_Mapper:
             distance_scores = 1 - (distances - distances.min()) / (distances.max() - distances.min() + 1e-6)
             for i, frontier in enumerate(self.frontier_points):
                 distance = pointcloud_2d_distance(self.navigable_pcd,self.transform_world_to_pcd(frontier))
-                affordance[distance <= 0.1] = distance_scores[i]
+                affordance[distance <= 0.2] = distance_scores[i]
             return affordance
         elif action == 'LLM':
             affordance = np.zeros((self.navigable_pcd.point.positions.shape[0],),dtype=np.float32)
@@ -298,10 +298,10 @@ class Instruct_Mapper:
             current_world_position[[1, 2]] = current_world_position[[2, 1]]
             distances = np.linalg.norm(current_world_position - np.array(self.frontier_centers), axis=1)
             distance_scores = 1 - (distances - distances.min()) / (distances.max() - distances.min() + 1e-6)
-            scores = llm_scores * 0.7 + distance_scores * 0.3
+            scores = (llm_scores + distance_scores) / 2
             for i, frontier in enumerate(self.frontier_points):
                 distance = pointcloud_2d_distance(self.navigable_pcd,self.transform_world_to_pcd(frontier))
-                affordance[distance <= 0.1] = scores[i]
+                affordance[distance <= 0.2] = scores[i]
             return affordance
         elif action == 'LLM_Room':
             frontier_index = self.llm_agent.choose_cluster(self.object_clusters)
@@ -490,6 +490,10 @@ class Instruct_Mapper:
     def calculate_frontiers(self, dbscan_eps=1.0, dbscan_min_samples=1):
 
         world_points = self.transform_pcd_to_world(self.frontier_pcd)
+
+        # Remove frontier points within 1.0 meter distance of the current position
+        distances = np.linalg.norm(world_points[:, [0, 2]] - (self.current_position + self.initial_position)[[0, 1]], axis=1)
+        world_points = world_points[distances > 1.0]
 
         if world_points.shape[0] == 0:
             return [], [], []
