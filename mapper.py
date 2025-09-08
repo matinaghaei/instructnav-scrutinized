d5f68904f8f4ae52cc60173db132745a905b45e2
Matin <m.jaffaraghaei@gmail.com>
2025-09-08 11:44:23 -0400

    Revert back to non-distance-based Frontier and LLM agents

--- mapper.py
+++ mapper.py
@@ -271,14 +271,14 @@ class Instruct_Mapper:
         # try:
         if (action == 'LLM' or action == 'LLM_Room') and (not self.object_entities or not self.object_clusters):
             action = 'Frontier'
-        if action == 'Explore':
+        if action == 'Frontier':
             if self.frontier_pcd.is_empty():
                 return np.zeros((self.navigable_pcd.point.positions.shape[0],),dtype=np.float32)
             distance = pointcloud_2d_distance(self.navigable_pcd,self.frontier_pcd)
             affordance = 1 - (distance - distance.min()) / (distance.max() - distance.min() + 1e-6)
             affordance[distance > 0.2] = 0
             return affordance.cpu().numpy()
-        if action == 'Frontier':
+        elif action == 'Distance-based Frontier':
             affordance = np.zeros((self.navigable_pcd.point.positions.shape[0],),dtype=np.float32)
             if not self.frontier_centers:
                 return affordance
@@ -288,20 +288,16 @@ class Instruct_Mapper:
             distance_scores = 1 - (distances - distances.min()) / (distances.max() - distances.min() + 1e-6)
             for i, frontier in enumerate(self.frontier_points):
                 distance = pointcloud_2d_distance(self.navigable_pcd,self.transform_world_to_pcd(frontier))
-                affordance[distance <= 0.2] = distance_scores[i]
+                distance_affordance = 1 - (distance - distance.min()) / (distance.max() - distance.min() + 1e-6)
+                affordance[distance <= 0.2] = distance_affordance[distance <= 0.2] * distance_scores[i]
             return affordance
         elif action == 'LLM':
             affordance = np.zeros((self.navigable_pcd.point.positions.shape[0],),dtype=np.float32)
             llm_scores = self.llm_agent.score_clusters(self.object_clusters)
             llm_scores = (llm_scores - llm_scores.min()) / (llm_scores.max() - llm_scores.min() + 1e-6)
-            current_world_position = self.current_position + self.initial_position
-            current_world_position[[1, 2]] = current_world_position[[2, 1]]
-            distances = np.linalg.norm(current_world_position - np.array(self.frontier_centers), axis=1)
-            distance_scores = 1 - (distances - distances.min()) / (distances.max() - distances.min() + 1e-6)
-            scores = (llm_scores + distance_scores) / 2
             for i, frontier in enumerate(self.frontier_points):
                 distance = pointcloud_2d_distance(self.navigable_pcd,self.transform_world_to_pcd(frontier))
-                affordance[distance <= 0.2] = scores[i]
+                affordance[distance <= 0.2] = llm_scores[i]
             return affordance
         elif action == 'LLM_Room':
             frontier_index = self.llm_agent.choose_cluster(self.object_clusters)
@@ -492,8 +488,8 @@ class Instruct_Mapper:
         world_points = self.transform_pcd_to_world(self.frontier_pcd)
 
         # Remove frontier points within 1.0 meter distance of the current position
-        distances = np.linalg.norm(world_points[:, [0, 2]] - (self.current_position + self.initial_position)[[0, 1]], axis=1)
-        world_points = world_points[distances > 1.0]
+        # distances = np.linalg.norm(world_points[:, [0, 2]] - (self.current_position + self.initial_position)[[0, 1]], axis=1)
+        # world_points = world_points[distances > 1.0]
 
         if world_points.shape[0] == 0:
             return [], [], [], []
